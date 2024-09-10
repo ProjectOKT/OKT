618eac1f970f84a4eef72c1b006cff9f876c535b
Kim jae Hyo <jejae0718@kookmin.ac.kr>
2024-09-10 15:13:44 +0900

    test2

--- test.c
+++ test.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-int main(){
-    char str[4] = "Test";
-    for(int i = 0; i < 4; i ++){
-        printf("%s[%d]\n", str, i + 1);
-    }
-}
+// int main(){
+//     char str[4] = "Test";
+//     for(int i = 0; i < 4; i ++){
+//         printf("%s[%d]\n", str, i + 1);
+//     }
+// }
---
+++ test2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int a = 11;
+    unsigned char k = 0x22;
+
+    printf("%d \n", a);
+    printf("%u", k);
+    return 0;
+}
