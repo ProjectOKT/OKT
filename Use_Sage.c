57b8714188b4fc10003c3ee56757e73d3a62b80a
Kim jae Hyo <jejae0718@kookmin.ac.kr>
2024-09-11 10:26:52 +0900

    Sample Sage

---
+++ Use_Sage.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/************************
+Use_Sage : Sample of Using Sage operating
+************************/
+
+int Use_Sage() {
+    char buffer[500];   //크기 조절 필요
+    FILE *fp;
+    int result;
+
+    //Sage 실행, 출력 가져오기
+    fp = popen("sage -c 'print(2^128)'", "r");
+    if (fp == NULL) {
+        printf("NULL Error!\n");
+        return 1;
+    }
+
+    // 명령어 결과를 읽어와서 저장
+    fgets(buffer, sizeof(buffer), fp);
+    pclose(fp);
+
+    //c에서의 자료형으로 나타낼 수 없으므로 문자열로 출력
+    printf("SageMath에서 계산된 값: %s\n", buffer);
+
+    return 0;
+}
+
+int main(){
+    Use_Sage();
+    return 0;
+}
