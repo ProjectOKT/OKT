9fbc31311af8bfb15a273a8305547722bd2f4b75
Kim jae Hyo <jejae0718@kookmin.ac.kr>
2024-10-17 01:18:05 +0900

    오타수정_Oct_17

--- verify.c
+++ verify.c
@@ -4,7 +4,7 @@
 void check_leaks(){
 
     #if defined(_WIN32) || defined(_WIN64)
-        system("rmemory -- 2024_bigint.exe");
+        system("drmemory -- 2024_bigint.exe");
     #elif defined(__APPLE__) || defined(__MACH__)
         system("leaks 2024_bigint");
     #else
