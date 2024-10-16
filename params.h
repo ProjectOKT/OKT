69ea49fc8778256bfc72854da67fc9146f2fa8ef
Kim-yong-jae <rladydwocjsw@kookminl.ac.kr>
2024-10-16 17:13:53 +0900

    sizeofword추가

--- params.h
+++ params.h
@@ -7,6 +7,6 @@
 #define NEGATIVE  -1
 #define ZERORIZE   1 // Unsecure delete: 0, Secure delete: 1 
 #define ENDIAN     0 // Little endian: 0, Big endian: 1
-
+#define SIZEOFWORD 32 //bitsize of word
 
 #endif
