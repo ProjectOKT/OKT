b4506d8302d5695c970d46a6e7ffc34be7024960
Kim-yong-jae <rladydwocjsw@kookminl.ac.kr>
2024-10-16 17:05:34 +0900

    wordsize추가

--- bigintfun.c
+++ bigintfun.c
@@ -41,74 +41,141 @@ msg bi_set_from_array(bigint** dst, int sign, int word_len, word* a)
  *          - char* int_str : bigint로 변환하려는 string
  * Return : (SUCCESS) bigint print
 ******************************************/
-msg bi_set_from_string(bigint** dst, int sign, char* int_str, int base)
+msg bi_set_from_string(bigint** dst, const char* int_str, int base)
 {
     int word_len = 0;
-    unsigned int strlength = 0; //0b,0x, ... 뺴고 계산
-    if(base == 2){
-        strlength = strlen(int_str); //string 길이
-        word_len = strlength % 32 == 0 ? strlength / 32 : strlength / 32 +1;  //word 길이
-        //구한 word len으로 bigint 하나 만들기
-        bi_new(dst, word_len);
-        (*dst) -> sign = sign;
-        //비트열 길이만큼 반복
-        for (int i = 0; i < strlength - 2; i++){
-            if (int_str[strlength - i - 1] == '1'){
-                //1이면 32의 나머지만큼 shift해서 32로 나눈몫에저장
-                (*dst) -> a[i/32] |= 1 << (i % 32);
-            }
-            else if (int_str[strlength - i - 1] != '0'){
-                fprintf(stderr, ERR_INVALID_INPUT);
-                bi_delete(dst);
-                return FAILED;
+    unsigned int strlength = 0; 
+    if (base == 2){
+        if (int_str[0] == '-'){
+            strlength = strlen(int_str) - 1; //string 숫자 길이
+            word_len = strlength % SIZEOFWORD == 0 ? strlength / SIZEOFWORD : strlength / SIZEOFWORD +1;  //word 길이
+            //구한 word len으로 bigint 하나 만들기
+            bi_new(dst, word_len);
+            (*dst) -> sign = NEGATIVE;
+            for (int i = 0; i < strlength; i++){
+                if (int_str[strlength - i] == '1'){
+                    //1이면 32의 나머지만큼 shift해서 64로 나눈몫에저장
+                    (*dst) -> a[i/SIZEOFWORD] |= 1 << (i % SIZEOFWORD);
+                }
+                else if (int_str[strlength - i] != '0'){
+                    fprintf(stderr, ERR_INVALID_INPUT);
+                    bi_delete(dst);
+                    return FAILED;
+                }
             }
         }
+        else{
+            strlength = strlen(int_str); //string 숫자 길이
+            word_len = strlength % SIZEOFWORD == 0 ? strlength / SIZEOFWORD : strlength / SIZEOFWORD +1;  //word 길이
+            //구한 word len으로 bigint 하나 만들기
+            bi_new(dst, word_len);
+            (*dst) -> sign = POSITIVE;
+            for (int i = 0; i < strlength; i++){
+                if (int_str[strlength - i - 1] == '1'){
+                    //1이면 SIZEOFWORD의 나머지만큼 shift해서 SIZEOFWORD로 나눈몫에저장
+                    (*dst) -> a[i/SIZEOFWORD] |= 1 << (i % SIZEOFWORD);
+                }
+                else if (int_str[strlength - i - 1] != '0'){
+                    fprintf(stderr, ERR_INVALID_INPUT);
+                    bi_delete(dst);
+                    return FAILED;
+                }
+            }
+        }
+        
     }
     else if (base == 8){
-        strlength = strlen(int_str);  // 0을 제외한 길이
-        word_len = strlength * 3 % 32 == 0 ? strlength * 3/ 32 : strlength * 3 / 32 +1;  // 8진수는 3비트로 표현되므로 3비트씩 계산
-        bi_new(dst, word_len);
-        (*dst)->sign = sign;
-
-         for (int i = 0; i < strlength - 1; i++) {
-            if (int_str[strlength - i - 1] >= '0' && int_str[strlength - i - 1] < '8') {
-                int digit = int_str[strlength - i - 1] - '0';  // 문자 숫자를 정수로 변환
-                (*dst)->a[i * 3 / 32] |= (digit & 0x7) << (i * 3 % 32);  // 3비트씩 shift하여 저장
-            }
-            else{
-                fprintf(stderr, ERR_INVALID_INPUT);
-                bi_delete(dst);
-                return FAILED;
+        if (int_str[0] == '-'){
+            strlength = strlen(int_str)-1;  // 0을 제외한 길이
+            word_len = strlength * 3 % SIZEOFWORD == 0 ? strlength * 3/ SIZEOFWORD : strlength * 3 / SIZEOFWORD +1;  // 8진수는 3비트로 표현되므로 3비트씩 계산
+            bi_new(dst, word_len);
+            (*dst)->sign = NEGATIVE;
+            for (int i = 0; i < strlength; i++) {
+                if (int_str[strlength - i] >= '0' && int_str[strlength - i] < '8') {
+                    int digit = int_str[strlength - i] - '0';  // 문자 숫자를 정수로 변환
+                    (*dst)->a[i * 3 / SIZEOFWORD] |= (digit & 0x7) << (i * 3 % SIZEOFWORD);  // 3비트씩 shift하여 저장
+                }
+                else{
+                    fprintf(stderr, ERR_INVALID_INPUT);
+                    bi_delete(dst);
+                    return FAILED;
+                }
             }
         }
+        else {
+            strlength = strlen(int_str);  // 0을 제외한 길이
+            word_len = strlength * 3 % SIZEOFWORD == 0 ? strlength * 3/ SIZEOFWORD : strlength * 3 / SIZEOFWORD +1;  // 8진수는 3비트로 표현되므로 3비트씩 계산
+            bi_new(dst, word_len);
+            (*dst)->sign = POSITIVE;
+            for (int i = 0; i < strlength; i++) {
+                if (int_str[strlength - i - 1] >= '0' && int_str[strlength - i - 1] < '8') {
+                    int digit = int_str[strlength - i - 1] - '0';  // 문자 숫자를 정수로 변환
+                    (*dst)->a[i * 3 / SIZEOFWORD] |= (digit & 0x7) << (i * 3 % SIZEOFWORD);  // 3비트씩 shift하여 저장
+                }
+                else{
+                    fprintf(stderr, ERR_INVALID_INPUT);
+                    bi_delete(dst);
+                    return FAILED;
+                }
+            }
+        }
+        
+
+         
     }
     else if (base == 10){
-        strlength = strlen(int_str) -2;
+        return 0;
     }
     else if (base == 16){
-        strlength = strlen(int_str);  // 0x를 제외한 길이
-        word_len = strlength * 4 % 32 == 0 ? strlength * 4 / 32 : strlength * 4 / 32 +1;  
-        bi_new(dst, word_len);
-        (*dst)->sign = sign;
+        if (int_str[0] == '-'){
+            strlength = strlen(int_str)-1;  // 0x를 제외한 길이
+            word_len = strlength * 4 % SIZEOFWORD == 0 ? strlength * 4 / SIZEOFWORD : strlength * 4 / SIZEOFWORD +1;  
+            bi_new(dst, word_len);
+            (*dst)->sign = NEGATIVE;
+            for (int i = 0; i < strlength; i++) {
+                char c = int_str[strlength - i];
+                int digit = 0;
 
-        for (int i = 0; i < strlength - 2; i++) {
-            char c = int_str[strlength - i - 1];
-            int digit = 0;
+                // 16진수 문자를 정수로 변환
+                if (c >= '0' && c <= '9') {
+                    digit = c - '0';
+                } else if (c >= 'a' && c <= 'f') {
+                    digit = c - 'a' + 10;
+                } else if (c >= 'A' && c <= 'F') {
+                    digit = c - 'A' + 10;
+                }
+                else{
+                    fprintf(stderr, ERR_INVALID_INPUT);
+                    bi_delete(dst);
+                    return FAILED;
+                }
+                (*dst)->a[i * 4 / SIZEOFWORD] |= (digit & 0xF) << (i * 4 % SIZEOFWORD);  // 4비트씩 shift하여 저장
+            }
+        }
+        else {
+            strlength = strlen(int_str); 
+            word_len = strlength * 4 % SIZEOFWORD == 0 ? strlength * 4 / SIZEOFWORD : strlength * 4 / SIZEOFWORD +1;  
+            bi_new(dst, word_len);
+            (*dst)->sign = POSITIVE;
+            for (int i = 0; i < strlength; i++) {
+                char c = int_str[strlength - i - 1];
+                int digit = 0;
 
-            // 16진수 문자를 정수로 변환
-            if (c >= '0' && c <= '9') {
-                digit = c - '0';
-            } else if (c >= 'a' && c <= 'f') {
-                digit = c - 'a' + 10;
-            } else if (c >= 'A' && c <= 'F') {
-                digit = c - 'A' + 10;
+                // 16진수 문자를 정수로 변환
+                if (c >= '0' && c <= '9') {
+                    digit = c - '0';
+                } else if (c >= 'a' && c <= 'f') {
+                    digit = c - 'a' + 10;
+                } else if (c >= 'A' && c <= 'F') {
+                    digit = c - 'A' + 10;
+                }
+                else{
+                    fprintf(stderr, ERR_INVALID_INPUT);
+                    bi_delete(dst);
+                    return FAILED;
+                }
+                (*dst)->a[i * 4 / SIZEOFWORD] |= (digit & 0xF) << (i * 4 % SIZEOFWORD);  // 4비트씩 shift하여 저장
             }
-            else{
-                fprintf(stderr, ERR_INVALID_INPUT);
-                bi_delete(dst);
-                return FAILED;
-            }
-            (*dst)->a[i * 4 / 32] |= (digit & 0xF) << (i * 4 % 32);  // 4비트씩 shift하여 저장
         }
     }
     else {
--- params.h
+++ params.h
@@ -5,3 +5,4 @@
 #define NEGATIVE -1
 #define ZERORIZE 1 // Unsecure delete: 0, Secure delete: 1 
 #define ENDIAN 0 // Little endian: 0, Big endian: 1
+#define SIZEOFWORD 32 //bit size of word 
