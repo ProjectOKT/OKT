d741507f14d152ab12c42783b64c60cc08aff707
oh-bin <01sungbin@kookmin.ac.kr>
2024-10-17 09:31:18 +0900

    오타수정_Oct_17

--- bigintfun.c
+++ bigintfun.c
@@ -229,7 +229,7 @@ msg bi_get_random(bigint** dst, int sign, int word_len)
  * 
  * Return : (SUCCESS)진수변환된 bigint print
 ******************************************/
-msg bi_print(const bigint** dst, int base)
+msg bi_print(bigint** dst, int base)
 {
     if((*dst)->sign == ZERO) // sign ZERO면 ZERO출력 후 반환
     {
